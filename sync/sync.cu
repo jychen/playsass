f0c2e542077e1a634a4fa8f163727a24ca562b66
chenjianye <jychen84@gmail.com>
2024-12-31 16:11:34 +0800

    warpsync/groupsync

--- sync/sync.cu
+++ sync/sync.cu
@@ -1,4 +1,6 @@
 #include "cuda.h"
+#include <cooperative_groups.h>
+#include <cooperative_groups/memcpy_async.h>
 
 __global__ void syncthreads_kernel()
 {
@@ -9,6 +11,21 @@ __global__ void threadfence_kernel()
 {
     __threadfence();
 }
+
+__global__ void groupsync_kernel(int *a, int b)
+{
+    auto g = cooperative_groups::this_grid();
+    g.sync();
+    if (threadIdx.x == 0) *a = b;
+}
+
+__global__ void warpsync_kernel(int *a)
+{
+    auto warp = cooperative_groups::coalesced_threads();
+    warp.sync();
+    if (threadIdx.x == 0) *a = 100;
+}
+
 int main(int argc, char **argv)
 {
     return 0;
